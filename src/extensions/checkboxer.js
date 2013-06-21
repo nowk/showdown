37d942f6ce4d37ca0de38130c40a7f89a07fd54e
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2013-06-21 03:50:39 -0400

    Easier access to filter Strip uuid

--- src/extensions/checkboxer.js
+++ src/extensions/checkboxer.js
@@ -2,31 +2,38 @@
 //
 
 (function() {
-  var checkboxer = function(converter) {
-    var generate_uuid = function() {
+  var checkboxer_methods = {
+    generate_uuid:function() {
       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c){
         r = Math.random()*16|0;
         v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
       });
-    };
+    },
+    uuidify: function(text, strip) {
+      var whole_list = /(\s|\n)?(\s{0,3})(\\)?([*+\-]|\d+[.])[ \t]+(\[(\s|\*)\])(\[([\w\-]+)\]+)?/gi;
 
+      return text.replace(whole_list, 
+        function(match, m1, m2, m3, marker, checked_attr, checked, uuid_attr, uuid) {
+        // console.log( m1, m2, m3, marker, checked_attr, checked, uuid_attr, uuid);
+        if (uuid === '' || (typeof uuid === 'undefined')) {
+          match = match+'['+checkboxer_methods.generate_uuid()+']';
+        } else {
+          if (strip === true) {
+             match = [m1, m2, m3, marker, ' ', checked_attr].join("");
+          }
+        }
+        return match;
+      });
+    }
+  };
+
+  var checkboxer = function(converter) {
     return [
       {
         name: 'preparser',
         type: 'lang', 
-        filter: function(text) {
-          var whole_list = /(\s|\n)?(\s{0,3})(\\)?([*+\-]|\d+[.])[ \t]+\[(\s|\*)\](\[([\w\-]+)\]+)?/gi;
-
-          return text.replace(whole_list, 
-            function(match, m1, m2, m3, marker, checked, m4, uuid, m5) {
-            if (uuid === '' || (typeof uuid === 'undefined')) {
-              match = match+'['+generate_uuid()+']';
-            }
-
-            return match;
-          });
-        }
+        filter: checkboxer_methods.uuidify
       },
       {
         type: 'output',
@@ -35,18 +42,17 @@
           return source.replace(/((<li>)(<p>)?)(\[\s\]|\[\*\]|\[<\/?em>\])(\[([\w\-]+)\])(\s?[\w\s]+)/gi, 
             function(match, lip, li, p, checkmark, uuid_attr, uuid, content) {
               // console.log(lip, li, p, checkmark, uuid_attr, uuid, content);
-
               if (
                 (li == '<li>') &&
                 (checkmark == '[ ]' || checkmark == '[*]' || checkmark == '[<em>]' || checkmark == '[</em>]')
-               ) {
+                ) {
+
                 lip = '<li class="li-checkbox">'+(p||"");
                 checked = checkmark == '[*]' || checkmark == '[<em>]' || checkmark == '[</em>]' ? 
                   'checked="checked"' : "";
                 return lip+'<input type="checkbox" '+checked+
                   ' class="checkboxer-checkbox" data-checkboxer-uuid="'+uuid+'" /> '+content;
               }
-
               return match;
             }
           );
@@ -56,7 +62,13 @@
   };
 
   // Client-side export
-  if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { window.Showdown.extensions.checkboxer = checkboxer; }
+  if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { 
+    checkboxer.filter = {
+      uuidify: checkboxer_methods.uuidify
+    };
+
+    window.Showdown.extensions.checkboxer = checkboxer; 
+  }
   // Server-side export
   if (typeof module !== 'undefined') module.exports = checkboxer;
 }());
